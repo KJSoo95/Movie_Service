e78ec419cbfd38875ab99b14bc5312b1f4661639
KJSoo95 <wltn7star@naver.com>
2024-04-23 20:30:55 +0900

    feat : useEffect practie

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,32 @@
 import "./App.css";
 import Button from "./Button";
-
+import { useEffect, useState } from "react";
 
 function App() {
+  const [counter, setCounter] = useState(0);
+  const [keyword, setKeyword] = useState("");
+
+  const onClick = () => {
+    setCounter((prev) => prev + 1);
+  };
+
+  const onChange = (e) => {
+    setKeyword(e.target.value);
+  };
+
+  useEffect(() => {
+    console.log("search", keyword);
+  }, [keyword]);
+
+  useEffect(() => {
+    console.log("count");
+  }, [counter]);
 
   return (
     <>
-      <div>hi</div>
+      <input value={keyword} onChange={onChange} placeholder="search here" />
+      <div>{counter}</div>
+      <button onClick={onClick}>Click me</button>
       <Button text="Hi" />
     </>
   );
